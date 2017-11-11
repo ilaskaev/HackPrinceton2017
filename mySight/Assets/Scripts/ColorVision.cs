d3b7b6bce2f3c91d3f67c01605da07ef33164c7b
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2017-11-11 14:17:50 -0500

    Added buttons to main menu

---
+++ mySight/Assets/Scripts/ColorVision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColorVision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene("Color Vision");
+	}
+}
---
+++ mySight/Assets/Scripts/VisualAcuity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VisualAcuity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene("visual-acuity");
+	}
+}
