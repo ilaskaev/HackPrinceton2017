0764a303b5560eb7c0fef755c94473c019bec115
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2017-11-11 16:52:42 -0500

    Added colour-vision to main menu

---
+++ mySight/Assets/GoogleVR/Scripts/ColourVision/ColourVision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourVision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("colour-vision");
+	}
+}
