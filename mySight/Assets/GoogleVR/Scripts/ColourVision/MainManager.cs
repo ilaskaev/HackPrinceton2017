84d4cda9856cc9769b32ea4d2621d46f0c251226
Jesse-Daniel Belleau-Karaskewich <jdbkaraskewich@gmail.com>
2017-11-11 06:57:33 -0500

    Timer works!

---
+++ mySight/Assets/GoogleVR/Scripts/ColourVision/MainManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour {
+
+    public GameObject plate;
+    public PlateManager plateManager;
+    public GameObject quizCanvas;
+    public GameObject instructionCanvas;
+	// Use this for initialization
+	void Start () {
+        plate.SetActive(false);
+        quizCanvas.SetActive(false);
+        instructionCanvas.SetActive(true);
+	}
+
+    public void ContinuePressed()
+    {
+        instructionCanvas.SetActive(false);
+        plate.SetActive(true);
+        StartCoroutine(ShowPictureFor(5));
+    }
+
+    private IEnumerator ShowPictureFor(int number)
+    {
+        yield return new WaitForSeconds(5);
+        plate.SetActive(false);
+        quizCanvas.SetActive(true);
+
+    }
+}
