73475268719778683d21edaa45326a16a951336f
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2017-11-11 21:59:56 -0500

    Added stereopsis functionality

---
+++ mySight/Assets/GoogleVR/Scripts/Stereopsis/Stereopsis.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stereopsis : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("stereopsis");	
+	}
+}
---
+++ mySight/Assets/GoogleVR/Scripts/VisualAcuity/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main-menu");
+	}
+}
